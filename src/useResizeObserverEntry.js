2377e6c0ca0a7af82b7684f52d2377ecdbafa2d6
Marc Dingena <marc.dingena@gmail.com>
2019-12-05 15:28:51 +1100

    Create useResizeObserverEntry.js

---
+++ src/useResizeObserverEntry.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { Context } from './Context';
+
+/**
+ * Returns the ResizeObserverEntry from nearest Context.
+ * @returns {(ResizeObserverEntry|null)}
+ */
+const useResizeObserverEntry = () => {
+  const [resizeObserverEntry] = useContext(Context);
+
+  return resizeObserverEntry;
+};
+
+export { useResizeObserverEntry };
