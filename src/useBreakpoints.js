e9de344f01f4fec31ed57b150dfce116fc589871
Marc Dingena <marc.dingena@gmail.com>
2019-12-05 16:02:50 +1100

    Change useBreakpoints API

--- src/useBreakpoints.js
+++ src/useBreakpoints.js
@@ -1,5 +1,11 @@
-import { useContext, useState, useEffect } from 'react';
-import { Context } from './Context';
+import { useState, useEffect } from 'react';
+import { useResizeObserverEntry } from './useResizeObserverEntry';
+
+const boxOptions = {
+  BORDER_BOX: 'border-box', // https://caniuse.com/#feat=mdn-api_resizeobserverentry_borderboxsize
+  CONTENT_BOX: 'content-box', // https://caniuse.com/#feat=mdn-api_resizeobserverentry_contentboxsize
+  DEVICE_PIXEL_CONTENT_BOX: 'device-pixel-content-box' // https://github.com/w3c/csswg-drafts/issues/3554
+};
 
 /**
  * Find the breakpoint matching the given cell size.
@@ -25,16 +31,49 @@ const findBreakpoint = (breakpoints, cellSize) => {
  * @param {Object} heightMap - Map of heights and their return values.
  * @returns {Array} Array of matching width breakpoint, and matching height breakpoint.
  */
-const useBreakpoints = (widthMap = {}, heightMap = {}) => {
-  const [cellWidth, cellHeight] = useContext(Context);
+const useBreakpoints = ({
+  widths = {},
+  heights = {},
+  box = undefined
+} = {}) => {
+  const resizeObserverEntry = useResizeObserverEntry();
 
-  const [width, setWidth] = useState(findBreakpoint(widthMap, cellWidth));
-  const [height, setHeight] = useState(findBreakpoint(heightMap, cellHeight));
+  /**
+   * Short-circuit if element from Context has not been observed yet.
+   * This is mostly the case when doing Server-Side Rendering.
+   */
+  if (!resizeObserverEntry) return [undefined, undefined];
+
+  let entryWidth, entryHeight;
+
+  switch (box) {
+    case boxOptions.BORDER_BOX:
+      entryWidth = resizeObserverEntry.borderBoxSize.inlineSize;
+      entryHeight = resizeObserverEntry.borderBoxSize.blockSize;
+      break;
+
+    case boxOptions.CONTENT_BOX:
+      entryWidth = resizeObserverEntry.contentBoxSize.inlineSize;
+      entryHeight = resizeObserverEntry.contentBoxSize.blockSize;
+      break;
+
+    case boxOptions.DEVICE_PIXEL_CONTENT_BOX:
+      entryWidth = resizeObserverEntry.devicePixelContentBoxSize.inlineSize;
+      entryHeight = resizeObserverEntry.devicePixelContentBoxSize.blockSize;
+      break;
+
+    default:
+      entryWidth = resizeObserverEntry.contentRect.width;
+      entryHeight = resizeObserverEntry.contentRect.height;
+  }
+
+  const [width, setWidth] = useState(findBreakpoint(widths, entryWidth));
+  const [height, setHeight] = useState(findBreakpoint(heights, entryHeight));
 
   useEffect(() => {
-    setWidth(findBreakpoint(widthMap, cellWidth));
-    setHeight(findBreakpoint(heightMap, cellHeight));
-  }, [widthMap, cellWidth, heightMap, cellHeight]);
+    setWidth(findBreakpoint(widths, entryWidth));
+    setHeight(findBreakpoint(heights, entryHeight));
+  }, [widths, entryWidth, heights, entryHeight]);
 
   return [width, height];
 };
