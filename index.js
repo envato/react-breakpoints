b86025ed71bb98f342f16581b4e0a6811c8119e9
Marc Dingena <marc.dingena@gmail.com>
2019-12-06 15:13:32 +1100

    Use microbundle again

---
+++ index.js
@@ -0,0 +1,5 @@
+export { Provider, useResizeObserver } from '@envato/react-resize-observer-hook';
+export { Observe } from './src/Observe';
+export { useBreakpoints } from './src/useBreakpoints';
+export { Context } from './src/Context';
+export { useResizeObserverEntry } from './src/useResizeObserverEntry';
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { Provider, useResizeObserver } from '@envato/react-resize-observer-hook';
-export { Observe } from './Observe';
-export { useBreakpoints } from './useBreakpoints';
-export { Context } from './Context';
-export { useResizeObserverEntry } from './useResizeObserverEntry';
