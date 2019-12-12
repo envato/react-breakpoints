85509cf47fe47b5c234ec70c039c495c7996c883
Marc Dingena <marc.dingena@gmail.com>
2019-12-12 13:42:06 +1100

    Update types to reflect new APIs

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,52 @@
-import { Context } from 'react';
+import React from 'react';
 
-interface IMap {
-  [key: number]: any;
-};
+interface ResizeObserverBoxSize {
+  readonly inlineSize: number
+  readonly blockSize: number
+}
 
-export const CellContext: Context<[number, number] | null>;
+interface ResizeObserverEntry {
+  readonly target: Element
+  readonly contentRect: DOMRectReadOnly
+  readonly borderBoxSize: ResizeObserverBoxSize
+  readonly contentBoxSize: ResizeObserverBoxSize
+  readonly devicePixelContentBoxSize: ResizeObserverBoxSize
+}
 
-export const useBreakpoints: (widthMap?: IMap, heightMap?: IMap) => [any, any];
+interface IObservedElementProps<T> {
+  ref: React.RefObject<T>
+}
+
+interface IObserveRenderProps<T> {
+  observedElementProps: IObservedElementProps<T>
+}
+
+interface IObserveProps<T> {
+  render: (props: IObserveRenderProps<T>) => React.ReactNode
+}
+
+interface IProvider {
+  value: ResizeObserverEntry | null
+}
+
+interface IBreakpoints<T> {
+  [key: number]: T
+}
+
+interface IOptions<W, H> {
+  widths?: IBreakpoints<W>
+  heights?: IBreakpoints<H>
+  box?: 'border-box' | 'content-box' | 'device-pixel-content-box'
+}
+
+export const Context: React.Context<ResizeObserverEntry | null>
+
+export const Observe: <T>(props: IObserveProps<T>) => React.ReactNode
+
+export const Provider: React.Context.Provider<IProvider>
+
+export const useBreakpoints: <W extends any, H extends any>(options: IOptions<W, H>) => [W, H]
+
+export const useResizeObserver: () => ResizeObserver | null
+
+export const useResizeObserverEntry: () => ResizeObserverEntry | null
