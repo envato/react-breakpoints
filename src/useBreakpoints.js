e25a38ab07c7340887d6790c0eb629e72498573e
Marc Dingena <marc.dingena@gmail.com>
2020-01-09 22:12:31 +1100

    Update documentation

--- src/useBreakpoints.js
+++ src/useBreakpoints.js
@@ -9,8 +9,8 @@ const boxOptions = {
 
 /**
  * Find the breakpoint matching the given entry size.
- * @param {Object} breakpoints - Map of sizes with their return values.
- * @param {Number} entrySize - Size of entry to check breakpoints against.
+ * @argument {Object} breakpoints - Map of sizes with their return values.
+ * @argument {Number} entrySize - Size of entry to check breakpoints against.
  * @returns {*} Value of `breakpoints` at matching breakpoint.
  */
 const findBreakpoint = (breakpoints, entrySize) => {
@@ -27,11 +27,12 @@ const findBreakpoint = (breakpoints, entrySize) => {
 
 /**
  * Returns mapped value for width and height of nearest Context.
- * @param {Object} options
- * @param {Object} [options.widths] - Map of minWidths and their return values.
- * @param {Object} [options.heights] - Map of minHeights and their return values.
- * @param {String} [options.box] - Name of element's box you want to match your breakpoints against. One of ['border-box', 'content-box', 'device-pixel-content-box'].
- * @param {Number} [options.fragment] - Index of fragment to return from array of observed box fragments.
+ * @argument {Object} options
+ * @argument {Object} [options.widths] - Map of minWidths and their return values.
+ * @argument {Object} [options.heights] - Map of minHeights and their return values.
+ * @argument {String} [options.box] - Name of element's box you want to match your breakpoints against. One of ['border-box', 'content-box', 'device-pixel-content-box'].
+ * @argument {Number} [options.fragment] - Index of fragment to return from array of observed box fragments.
+ * @argument {ResizeObserverEntry} [injectResizeObserverEntry] - Explicitly set the ResizeObserverEntry to use instead of fetching it from Context.
  * @returns {Array} Array of matching width value, and matching height value.
  */
 const useBreakpoints = ({
