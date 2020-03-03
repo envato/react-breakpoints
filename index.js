c69f9f1e8923c526f592ca30f4c66c8889e9d4bd
Marc Dingena <marc.dingena@gmail.com>
2020-03-03 11:45:09 +1100

    Add babel

--- index.js
+++
@@ -1,5 +0,0 @@
-export { Provider, useResizeObserver } from '@envato/react-resize-observer-hook';
-export { Observe } from './src/Observe';
-export { useBreakpoints } from './src/useBreakpoints';
-export { Context } from './src/Context';
-export { useResizeObserverEntry } from './src/useResizeObserverEntry';
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { Provider, useResizeObserver } from '@envato/react-resize-observer-hook';
+export { Observe } from './Observe';
+export { useBreakpoints } from './useBreakpoints';
+export { Context } from './Context';
+export { useResizeObserverEntry } from './useResizeObserverEntry';
