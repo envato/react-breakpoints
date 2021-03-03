1e60c468d731b1d179c885276880079233b82d97
Marc Dingena <marc.dingena@gmail.com>
2021-03-03 21:40:16 +0800

    Short-circuit if ResizeObserverEntry is undefined

--- src/findBreakpoint.ts
+++ src/findBreakpoint.ts
@@ -16,7 +16,9 @@ import { Breakpoints } from './Breakpoints';
  * // matchedValue => 'desktop'
  *
  */
-export const findBreakpoint = (breakpoints: Breakpoints, observedSize: number): any => {
+export const findBreakpoint = (breakpoints: Breakpoints, observedSize?: number): any => {
+  if (typeof observedSize === 'undefined') return undefined;
+
   let breakpoint: number | undefined;
   const sizes = Object.keys(breakpoints).map(key => Number(key));
 
